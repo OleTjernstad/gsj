4fbc6d52d59321d14eeaed2cff866edbb79ee7c5
Ole Tjernstad <ott556@gmail.com>
2023-01-31 22:29:27 +0100

    create hero section

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <div className={styles.video}>
+      <section className={styles.wrapper}>
         <video width="100%" autoPlay muted loop id="myVideo">
           <source
             src="https://glommasvingen.no/wp-content/uploads/2020/02/Do-you-hear-slutt.mp4"
@@ -11,7 +11,25 @@ export default function Home() {
           />
           Your browser does not support HTML5 video.
         </video>
-      </div>
+        <h1
+          className={
+            styles.videoSectionCenter + " " + styles.videoSectionCenterTitle
+          }
+        >
+          Glommasvingen Janitsjar
+        </h1>
+        <h2
+          className={
+            styles.videoSectionCenter + " " + styles.videoSectionCenterSubTitle
+          }
+        >
+          Det lokale korpset for Kongsvinger og Sør-Odal
+        </h2>
+      </section>
+      Glommasvingen Janitsjar det lokale korpset for Kongsvinger og Sør-Odal. Vi
+      øver onsdager på Skarnes Videregående skole. I tillegg kommer enkelte
+      seminarer og ekstraøvelser i forbindelse med konserter. Vi er rundt 25
+      medlemmer, men vi mangler akkurat deg!!
     </main>
   );
 }
