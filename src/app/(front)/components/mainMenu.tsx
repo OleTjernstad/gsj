daeda8817b1b009fec6f64226cf5cc7e80d034d5
Ole Tjernstad <ott556@gmail.com>
2023-02-02 22:45:04 +0100

    add dropdowns

--- src/app/(front)/components/mainMenu.tsx
+++ src/app/(front)/components/mainMenu.tsx
@@ -16,13 +16,61 @@ export function MainMenu() {
         <li>
           <a href="#">Hjem</a>
         </li>
+        <DropDown title="Dropdown menu 1" />
         <li>
           <a href="#">Om oss</a>
         </li>
+        <DropDown title="Dropdown menu" />
         <li>
           <a href="#">Kontakt oss</a>
         </li>
       </ul>
     </nav>
+  );
+}
+
+function DropDown({ title }: { title: string }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <li className="dropdown">
+      <button
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+        onClick={() => setIsOpen(!isOpen)}
+        type="button"
+        className="dropdown__title"
+        aria-expanded="false"
+        aria-controls={title.replace(/\s/gm, "-")}
+      >
+        {title}
+      </button>
+      <ul
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+        className={`dropdown__menu ${isOpen ? "dropdown__active" : ""}`}
+        id={title.replace(/\s/gm, "-")}
+      >
+        <li>
+          <a onClick={() => setIsOpen(!isOpen)} href="#">
+            Donuts
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setIsOpen(!isOpen)} href="#">
+            Cupcakes
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setIsOpen(!isOpen)} href="#">
+            Chocolate
+          </a>
+        </li>
+        <li>
+          <a onClick={() => setIsOpen(!isOpen)} href="#">
+            Bonbons
+          </a>
+        </li>
+      </ul>
+    </li>
   );
 }
