dcb5038f7d2528a6794ff61f2e7ae0975ecbeac3
Ole Tjernstad <ott556@gmail.com>
2023-02-05 09:50:28 +0100

    style hover

--- src/components/post-list/postList.tsx
+++ src/components/post-list/postList.tsx
@@ -5,25 +5,27 @@ import styles from "./postList.module.scss";
 export function PostListBox() {
   return (
     <article className={styles.article}>
-      <figure>
-        <Image
-          src={img}
-          alt=""
-          style={{
-            width: "100%",
-            height: "auto",
-            objectFit: "cover",
-          }}
-        />
-      </figure>
-      <div>
-        <h2>Lorem Ipsum</h2>
+      <a href="#">
+        <figure>
+          <Image
+            src={img}
+            alt=""
+            style={{
+              width: "100%",
+              height: "auto",
+              objectFit: "cover",
+            }}
+          />
+        </figure>
+        <div>
+          <h2>Lorem Ipsum</h2>
 
-        <p>
-          Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-          consectetur, adipisci velit...
-        </p>
-      </div>
+          <p>
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit...
+          </p>
+        </div>
+      </a>
       <div className={styles.bottom}>
         <div>
           Av <a href="#">Forfatter</a>
