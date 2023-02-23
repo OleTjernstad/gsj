1cc939cb29b923d4c9368a19c984f0f1edcbfb8a
Ole Tjernstad <ott556@gmail.com>
2023-02-23 20:41:39 +0100

    hover states

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export function Footer() {
   return (
     <footer className="app-footer">
@@ -10,7 +12,15 @@ export function Footer() {
       </address>
       <div>
         <address>Org. nr. 993 822 280</address>
-        <br /> <span className="copyright">&copy; Tjernstad Utvikling</span>
+        <br />
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href="https://info.tjernstad-utvikling.no/"
+          className="copyright"
+        >
+          &copy; Tjernstad Utvikling
+        </Link>
       </div>
     </footer>
   );
